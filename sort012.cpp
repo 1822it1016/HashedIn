93bc42152ba7d7a5ebea9196e464e837efe91bf3
1822it1016 <55509487+1822it1016@users.noreply.github.com>
2021-10-03 12:40:01 +0530

    Create sort012.cpp

---
+++ sort012.cpp
@@ -0,0 +1,23 @@
+
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        int m=0;
+        int h=nums.size()-1;
+        while(m<=h)
+        {
+            switch(nums[m])
+            {
+                case 0:
+                    swap(nums[l++],nums[m++]);
+                    break;
+                case 1:
+                    m++;
+                    break;
+                case 2:
+                    swap(nums[m],nums[h--]);
+                        break;
+                    
+            }
+        }
+        
+    }
